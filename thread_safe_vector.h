e1b3699a393a0bad1fc97b0865a360b5b3365428
QiaoXianJie(桌面事业部_语音交互技术中心) <qiaoxianjie@sogou-inc.com>
2018-06-11 18:36:25 +0800

    update

--- thread_safe_vector.h
+++ thread_safe_vector.h
@@ -4,12 +4,14 @@
 #include<mutex>
 #include<cstring>
 
+using size_type = unsigned int;
+
 template <class T>
 class  Vector {
 public:
   Vector();
-  Vector(unsigned int size);
-  Vector(unsigned int size, const T & initial);
+  Vector(size_type  size);
+  Vector(size_type  size, const T & initial);
   Vector(const Vector<T>& v);
   Vector(Vector<T>&& v);
   Vector(std::initializer_list<T> list);
@@ -19,143 +21,138 @@ public:
 
   void push_back(const T& value);
   void pop_back();
-  unsigned int size() const;
-  T& at(unsigned int index);
-
-  void _range_check(unsigned int index) const {
-    if (index >= _size) {
-      //should throw exception here.
-      //__throw_out_of_range("vector::_M_range_check");
-      std::cout << "vector::_M_range_check" << std::endl;
-    }
-  }
+  size_type  size() const;
+  T& at(size_type  index);
 
 private:
-  unsigned int _size;
-  unsigned int _capacity;
-  T* _buffer;
-  std::mutex _mutex;
+  size_type  m_size;
+  size_type  m_capacity;
+  T* m_buffer;
+  std::mutex m_mutex;
 };
 
 template<class T>
 Vector<T>::Vector() {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _capacity = 0;
-  _size = 0;
-  _buffer = 0;
+  std::lock_guard<std::mutex> lck(m_mutex);
+  m_capacity = 0;
+  m_size = 0;
+  m_buffer = 0;
 }
 
 template<class T>
-Vector<T>::Vector(unsigned int size) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _size = size;
-  unsigned int _bit = 0;
+Vector<T>::Vector(size_type  size) {
+  std::lock_guard<std::mutex> lck(m_mutex);
+  m_size = size;
+  size_type  _bit = 0;
   while(size != 0) {
     size = size >> 1;
     ++_bit;
   }
-  _capacity = 1 << _bit;
-  _buffer = new T[_capacity];
+  m_capacity = 1 << _bit;
+  m_buffer = new T[m_capacity];
 }
 
 template<class T>
-Vector<T>::Vector(unsigned int size, const T & initial) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _size = size;
-  unsigned int _bit = 0;
+Vector<T>::Vector(size_type  size, const T & initial) {
+  std::lock_guard<std::mutex> lck(m_mutex);
+  m_size = size;
+  size_type  _bit = 0;
   while(size != 0) {
     size = size >> 1;
     ++_bit;
   }
-  _capacity = 1 << _bit;
-  _buffer = new T[_capacity];
-  for (unsigned int i = 0; i < _size; i++)
-    _buffer[i] = initial;
+  m_capacity = 1 << _bit;
+  m_buffer = new T[m_capacity];
+  for (size_type  i = 0; i < m_size; i++)
+    m_buffer[i] = initial;
 }
 
 template<class T>
 Vector<T>::Vector(const Vector<T> & v) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _size = v._size;
-  _capacity = v._capacity;
-  _buffer = new T[_capacity];
-  for (unsigned int i = 0; i < _size; i++)
-    _buffer[i] = v._buffer[i];
+  std::lock_guard<std::mutex> lck(m_mutex);
+  m_size = v.m_size;
+  m_capacity = v.m_capacity;
+  m_buffer = new T[m_capacity];
+  for (size_type  i = 0; i < m_size; i++)
+    m_buffer[i] = v.m_buffer[i];
 }
 
 template<class T>
 Vector<T>::Vector(Vector<T> && v) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _capacity = std::move(v._capacity);
-  _size = std::move(v._size);
-  _buffer =std::move(v._buffer);
+  std::lock_guard<std::mutex> lck(m_mutex);
+  m_capacity = std::move(v.m_capacity);
+  m_size = std::move(v.m_size);
+  m_buffer =std::move(v.m_buffer);
 }
 
 template<class T>
 Vector<T>::Vector(std::initializer_list<T> list) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  unsigned int size = list.size();
-  _size = size;
-  unsigned int _bit = 0;
+  std::lock_guard<std::mutex> lck(m_mutex);
+  size_type  size = list.size();
+  m_size = size;
+  size_type  _bit = 0;
   while(size != 0) {
     size = size >> 1;
     ++_bit;
   }
-  _capacity = 1 << _bit;
-  _buffer = new T[_capacity];
+  m_capacity = 1 << _bit;
+  m_buffer = new T[m_capacity];
   int i = 0;
   for(auto value : list) 
-    _buffer[i++] = value;
+    m_buffer[i++] = value;
 }
 
 template<class T>
 Vector<T> & Vector<T>::operator = (const Vector<T> & v) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  delete[] _buffer;
-  _size = v._size;
-  _capacity = v._capacity;
-  _buffer = new T[_capacity];
-  for (unsigned int i = 0; i < _size; i++)
-    _buffer[i] = v._buffer[i];
+  std::lock_guard<std::mutex> lck(m_mutex);
+  delete[] m_buffer;
+  m_size = v.m_size;
+  m_capacity = v.m_capacity;
+  m_buffer = new T[m_capacity];
+  for (size_type  i = 0; i < m_size; i++)
+    m_buffer[i] = v.m_buffer[i];
   return *this;
 }
 
 template<class T>
 Vector<T>::~Vector() {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _size = 0;
-  _capacity = 0;
-  delete[] _buffer;
+  std::lock_guard<std::mutex> lck(m_mutex);
+  m_size = 0;
+  m_capacity = 0;
+  delete[] m_buffer;
 }
 
 template<class T>
 void Vector<T>::push_back(const T & v) {
-  std::lock_guard<std::mutex> lck(_mutex);
-  if (_size >= _capacity) {
-    _capacity = (_capacity == 0 ? 1 : _capacity << 1);
-    T* new_buffer = new T[_capacity];
-    memcpy(new_buffer, _buffer, _size * sizeof(T));
-    delete[] _buffer;
-    _buffer = new_buffer;
+  std::lock_guard<std::mutex> lck(m_mutex);
+  if (m_size >= m_capacity) {
+    m_capacity = (m_capacity == 0 ? 1 : m_capacity << 1);
+    T* new_buffer = new T[m_capacity];
+    memcpy(new_buffer, m_buffer, m_size * sizeof(T));
+    delete[] m_buffer;
+    m_buffer = new_buffer;
   }
-  _buffer[_size++] = v;
+  m_buffer[m_size++] = v;
 }
 
 template<class T>
 void Vector<T>::pop_back() {
-  std::lock_guard<std::mutex> lck(_mutex);
-  _size--;
+  std::lock_guard<std::mutex> lck(m_mutex);
+  if (m_size == 0)
+    throw std::out_of_range("pop_back from a empty vector!");
+  m_size--;
 }
 
 template<class T>
-unsigned int Vector<T>::size()const {
-  return _size;
+size_type  Vector<T>::size()const {
+  return m_size;
 }
 
 template<class T>
-T& Vector<T>::at(unsigned int index) {
-  _range_check(index);
-  return _buffer[index];
+T& Vector<T>::at(size_type  index) {
+  if (index >= m_size) 
+    throw std::out_of_range("index out of range!");
+  return m_buffer[index];
 }
 
 #endif
